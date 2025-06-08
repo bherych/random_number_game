0faba40e6c0ec7685b0e0ca39280b8deade436a7
bherych <bogdangerych@gmail.com>
2025-06-08 18:29:47 +0300

    added try except for guess and cleaned code

--- src/main.py
+++ src/main.py
@@ -3,27 +3,37 @@ import random
 def game(a, b, tries):
     random_number = random.randint(a, b)
     count = 1
-    print("Hello, World! I'm thinking of a number between {0} and {1}. Try to guess it. You have {2} tries.".format(a, b, tries))
+    print("Hello, World! I'm thinking of a number between {0} and {1}. " \
+    "Try to guess it. You have {2} tries.".format(a, b, tries))
     while True:
         if count > tries:
             print("Sorry but you lost. You exceeded the limits of tries of {0}.".format(tries))
             return
         
-        guess = int(input("Your guess: "))
-        if guess >= a and guess <= b:
+
+        try:
+            guess = int(input("Your guess: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+        
+
+        if a <= guess <= b:
             count += 1
             if guess == random_number:
                 print("Congratulations! You win!")
                 return
             elif guess < random_number:
                 print("Too small.")
-            elif guess > random_number:
+            else:
                 print("Too big.")
         else:
             print("Really? The random number is between {0} and {1}. Try again.".format(a, b))
-            continue
+
 
 def main():
     game(1, 100, 10)
 
-main()
+
+if __name__ == "__main__":
+    main()
